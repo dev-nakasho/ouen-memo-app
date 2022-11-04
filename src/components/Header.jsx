e6577320b1c25713c3eb3bd9b34e638f33cf4896
nakasho-win10 <shota9988@gmail.com>
2022-11-04 22:36:26 +0900

    headerレイアウト修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,18 @@
-// import { Button } from "./common/Button";
-
 export const Header = () => {
   return (
     <div
       style={{
         display: "flex",
-        justifyContent: "space-between",
+        flexDirection: "column",
+        justifyContent: "center",
         borderBottom: "1px dotted #ddd",
         height: "56px",
         lineHeight: "56px",
-        paddingLeft: "24px",
-        paddingRight: "24px",
+        paddingLeft: "16px",
         boxSizing: "border-box",
       }}
     >
-      <h3
-        style={{
-          color: "#999",
-          fontWeight: "normal",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-        }}
-      >
-        Memo App
-      </h3>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-        }}
-      ></div>
+      <h3 style={{ color: "#999" }}>Memo App</h3>
     </div>
   );
 };
